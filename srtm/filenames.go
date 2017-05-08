6327bdf4b9a39c8117c68fdedbc9878b8f3ee488
Thanasis Georgiou <contact@thgeorgiou.com>
2017-05-08 13:37:20 +0300

    Implement SRTM resource manager

--- srtm/filenames.go
+++ srtm/filenames.go
@@ -3,6 +3,7 @@ package SRTM
 import (
 	"fmt"
 	"strconv"
+	"github.com/sakisds/gigahertzor/coordinates"
 )
 
 // FilenameLengthError Error thrown when a .hgt file has an invalid name due to length
@@ -15,7 +16,7 @@ func (e *FilenameLengthError) Error() string {
 	return fmt.Sprintf("Filename %s has invalid length! (%d instead of 7)", e.Filename, e.length)
 }
 
-// FilenameToCoordinates Extract the coordinates from a .hgt filename
+// FilenameToCoordinates Returns the coordinates from the starting point of a .hgt filename
 func FilenameToCoordinates(filename string) (latitude float64, longitude float64, err error) {
 	// Check length
 	if len(filename) != 7 {
@@ -36,9 +37,28 @@ func FilenameToCoordinates(filename string) (latitude float64, longitude float64
 	if err != nil {
 		return 0.0, 0.0, err
 	}
-	if filename[3] == 'W' {
+	if filename[3] == 'W' { // Make negative if west
 		longitude *= -1
 	}
 
 	return
+}
+
+// CoordinatesToFilename returns the filename that covers the given point
+func CoordinatesToFilename(point coordinates.Point) string {
+	var NorthOrSouth, EastOrWest string
+
+	if point.Latitude >= 0 {
+		NorthOrSouth = "N"
+	} else {
+		NorthOrSouth = "S"
+	}
+
+	if point.Longitude >= 0 {
+		EastOrWest = "E"
+	} else {
+		EastOrWest = "W"
+	}
+
+	return fmt.Sprintf("%s%d%s%03d", NorthOrSouth, int(point.Latitude), EastOrWest, int(point.Longitude))
 }
